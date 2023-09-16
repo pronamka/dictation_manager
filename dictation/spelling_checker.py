35cf834465ee7ea9983e53967f405f02dd91b8f4
pronamka <defender0508@gmail.com>
2023-09-16 10:07:58 +0300

    adding support for strings like: `option1|option2_1/option2_2`(strings that contain different spellings of synonymous words)

--- dictation/spelling_checker.py
+++ dictation/spelling_checker.py
@@ -10,36 +10,41 @@ class SpellChecker(BaseChecker):
         self.transcription = f"The transcription is `{transcription}`" if transcription else ""
 
     def check(self):
-        if "n-" in self.words:
+        if "n-" in [j for i in self.words_variations for j in i]:
             return
         print(self.comment)
         self.get_answers()
 
     def get_answers(self):
-        if len(self.words) == 1 and self.words[0].find("|") != -1:
-            words = {i: 0 for i in self.words[0].split('|')}
+        amount_of_words_variations = len(self.words_variations)
+        if amount_of_words_variations > 1:
             sleep(1)
-            print(f"WARNING: There are {len(words)} variations. \n"
+            print(f"WARNING: There are {amount_of_words_variations} variations. \n"
                   f"You must give all of them.", file=sys.stderr)
-            while words:
-                used = self.wait_for_right_answer(list(words.keys()))
-                del words[used]
-        else:
-            self.wait_for_right_answer(self.words)
 
-    def wait_for_right_answer(self, words):
-        while (s := input()) not in words:
-            if s == "1":
+        self.wait_for_right_answer(self.words_variations)
+
+    def wait_for_right_answer(self, words: list[list]):
+        words_plain = {j: num for num, val in enumerate(words) for j in val}
+        words = {num: val for num, val in enumerate(words)}
+        while words:
+            answer = input()
+            answer_index = words_plain.get(answer, None)
+
+            if answer == "1":
                 print(f"The word is: {words[0]}")
-            elif s == "2":
+                return
+            elif answer == "2":
                 print(f"The word was: {words[0]}")
                 input("Now type it to remember.\n")
                 print(self.transcription)
                 print("OK. You will return to this word when you've "
                       "finished everything else.\n")
                 raise WordNotCompleted
-            else:
+            elif answer_index is None:
                 print("WRONG ANSWER", file=sys.stderr)
-        else:
-            print(f"RIGHT! {self.transcription}\n")
-            return s
+                continue
+            else:
+                print(f"RIGHT! {self.transcription}\n")
+                del words[answer_index]
+                words_plain = {key: val for key, val in words_plain.items() if val != answer_index}
