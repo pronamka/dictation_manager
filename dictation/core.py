ab292d551566c61e916da889ef8b0576d57d9b4a
pronamka <defender0508@gmail.com>
2023-09-23 02:48:04 +0300

    rewriting `SheetsSchemes` to get schemes from a `.txt` file and create `SheetScheme` instances

--- dictation/core.py
+++ dictation/core.py
@@ -29,91 +29,18 @@ class SheetScheme:
 
 
 class SheetsSchemes:
-    nouns = {"translation": 5,
-             "needs_revision": 7,
-             "to_check": [
-                 {"comment_spelling": "Type singular noun with definite article",
-                  "comment_pronunciation": "Pronounce singular noun without an article",
-                  "spelling": 0,
-                  "pronunciation": 1},
+    path_to_schemes = "sheets_schemes.txt"
 
-                 {"comment_spelling": "Type plural noun without an article",
-                  "comment_pronunciation": "Pronounce plural noun without an article",
-                  "spelling": 2,
-                  "pronunciation": 3}
-             ]}
+    def __init__(self):
+        self.all_schemes = {key: SheetScheme(*val) for key, val in self.read_schemes().items()}
+        self.available_sheets = list(self.all_schemes.keys())
 
-    verbs = {"translation": 3,
-             "needs_revision": 4,
-             "to_check": [
-                 {"comment_spelling": "Type the verb in the infinitive form",
-                  "comment_pronunciation": "Pronounce the verb in the infinitive form",
-                  "spelling": 0,
-                  "pronunciation": 1}
-             ]}
+    def read_schemes(self) -> dict:
+        with open(self.path_to_schemes, mode="r") as file:
+            return literal_eval(file.read())
 
-    irregular_verbs_present = {"translation": 10,
-                               "to_check": [
-                                   {"comment_spelling": "Type the verb in the infinitive form",
-                                    "spelling": 0,
-                                    "pronunciation": 1},
-                                   {"comment_spelling": "Type the verb in the present 1st person singular form",
-                                    "spelling": 2,
-                                    "pronunciation": 3},
-                                   {"comment_spelling": "Type the verb in the present 2nd person singular form",
-                                    "spelling": 4,
-                                    "pronunciation": 5},
-                                   {"comment_spelling": "Type the verb in the present 3rd person singular form",
-                                    "spelling": 6,
-                                    "pronunciation": 7},
-                                   {"comment_spelling": "Type the verb in the present 2nd person plural form",
-                                    "spelling": 8,
-                                    "pronunciation": 9},
-                               ]}
-
-    irregular_verbs_past = {"translation": 10,
-                               "to_check": [
-                                   {"comment_spelling": "Type the verb in the infinitive form",
-                                    "spelling": 0,
-                                    "pronunciation": 1},
-                                   {"comment_spelling": "Type the verb in the past 1st/3rd person singular form",
-                                    "spelling": 2,
-                                    "pronunciation": 3},
-                                   {"comment_spelling": "Type the verb in the past 2nd person singular form",
-                                    "spelling": 4,
-                                    "pronunciation": 5},
-                                   {"comment_spelling": "Type the verb in the past 1st/3rd person plural form",
-                                    "spelling": 6,
-                                    "pronunciation": 7},
-                                   {"comment_spelling": "Type the verb in the past 2nd person plural form",
-                                    "spelling": 8,
-                                    "pronunciation": 9},
-                               ]}
-
-    adjectives = {"translation": 3,
-                  "needs_revision": 4,
-                  "to_check": [
-                      {"comment_spelling": "Type the adjective: ",
-                       "comment_pronunciation": "Pronounce the adjective: ",
-                       "spelling": 0,
-                       "pronunciation": 1}
-                  ]}
-
-    other = {"translation": 3,
-             "needs_revision": 4,
-             "to_check": [
-                 {"comment_spelling": "Type the word(s): ",
-                  "comment_pronunciation": "Pronounce the word(s): ",
-                  "spelling": 0,
-                  "pronunciation": 1}
-             ]}
-
-    pronouns = {"translation": 3,
-                "to_check": [
-                    {"comment_spelling": "Type the word: ",
-                     "spelling": 0,
-                     "pronunciation": 1}
-                ]}
+    def get(self, key: str) -> SheetScheme:
+        return self.all_schemes.get(key, self.all_schemes.get("other"))
 
 
 class BaseChecker(ABC):
