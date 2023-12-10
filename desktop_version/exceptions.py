18143fe768bf7b8b888663c487d03f388fd63403
pronamka <defender0508@gmail.com>
2023-12-10 06:41:29 +0300

    adding `BaseExceptionWithUIMessage` abstract class and inheriting from it

--- desktop_version/exceptions.py
+++ desktop_version/exceptions.py
@@ -1,13 +1,24 @@
-class SchemeExistsError(Exception):
-    def __init__(self, scheme_name):
+from abc import ABC, abstractmethod
+
+
+class BaseExceptionWithUIMessage(Exception, ABC):
+    @abstractmethod
+    def message(self) -> str:
+        """Should return a string that will be displayed to the user in a label."""
+
+
+class SchemeExistsError(BaseExceptionWithUIMessage):
+    error_message = "Scheme with the name `{scheme_name}` already exists. " \
+                    "Give your scheme a different name."
+
+    def __init__(self, scheme_name: str) -> None:
         self.scheme_name = scheme_name
 
     def message(self):
-        return f"Scheme with the name `{self.scheme_name}` already exists. " \
-               f"Give your scheme a different name."
+        return self.error_message.format(self.scheme_name)
 
 
-class InvalidIndexesError(Exception):
+class InvalidIndexesError(BaseExceptionWithUIMessage):
     error_messages = ["Indexes of translation and status columns must be unique.",
                       "Indexes of word and information columns must not be the same."]
 
@@ -22,7 +33,7 @@ class InvalidIndexesError(Exception):
         return self.error_messages[1]
 
 
-class SheetNotFoundError(Exception):
+class SheetNotFoundError(BaseExceptionWithUIMessage):
     error_message = "The sheet name configured for that scheme is `{sheet_name}`, " \
                     "but that sheet is not present in the file `{file_path}`."
 
@@ -34,7 +45,7 @@ class SheetNotFoundError(Exception):
         return self.error_message.format(sheet_name=self.sheet_name, file_path=self.file_path)
 
 
-class VocabularyFileNotFoundError(FileNotFoundError):
+class VocabularyFileNotFoundError(BaseExceptionWithUIMessage):
     error_message = "File with vocabulary at the path: `{file_path}` was not found."
 
     def __init__(self, file_path: str):
@@ -55,11 +66,12 @@ class InvalidSchemeError(Exception):
         return self.error_message.format(sheet_name=self.sheet_name)
 
 
-class InvalidStatusError(Exception):
-    error_message = "An error occurred while processing the statuses in the status column of the sheet: `{sheet_name}`." \
-                    "Status column index was `{status_column_index}`; " \
-                    "Status that caused the error was `{status}` (on line `{line_index}`);" \
-                    "Check that the status name is allowed and that it has the index of power."
+class InvalidStatusError(BaseExceptionWithUIMessage):
+    error_message = "An error occurred while processing the statuses in the status " \
+                    "column of the sheet: `{sheet_name}`. \n" \
+                    "Status column index was `{status_column_index}`; \n" \
+                    "Status that caused the error was `{status}` (on line `{line_index}`); \n" \
+                    "Check that the status name is allowed and that it has the index of power(must be greater than 0)."
 
     def __init__(self, sheet_name: str, status_column_index: int, status: str, error_line_index: int):
         self.formatted_message = self.error_message.format(
